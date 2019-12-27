ce012597addb240f79c7ae36d685c52ce506ed57
wansf <854347546@qq.com>
2019-12-27 09:38:18 +0800

    Initial Commit

---
+++ GradientLabelDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradientLabelDemo
+//
+//  Created by tezwez on 2019/12/27.
+//  Copyright © 2019 tezwez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GradientLabelDemoTests/GradientLabelDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  GradientLabelDemoTests.swift
+//  GradientLabelDemoTests
+//
+//  Created by tezwez on 2019/12/27.
+//  Copyright © 2019 tezwez. All rights reserved.
+//
+
+import XCTest
+@testable import GradientLabelDemo
+
+class GradientLabelDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
